2b33c60a39da37ec90b7f65dbcd5287af4c3729b
evolvewithlivv <evolvewithlivv@gmail.com>
2026-09-06 11:05:29 -0400

    Wire official glossy ember PNG into EmberMark

--- src/components/brand/ember-src.ts
+++ src/components/brand/ember-src.ts
@@ -1,3 +1,2 @@
 /** Official glossy ember flame. Photoreal PNG, transparent. Do not flatten. */
-export const EMBER_SRC =
-  "https://raw.githubusercontent.com/evolvewithlivv/livv/main/public/ember.png";
+export const EMBER_SRC = "/api/ember";
