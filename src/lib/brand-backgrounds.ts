ddc7dedede08888ac33157eee102523e4f9bad28
evolvewithlivv <evolvewithlivv@gmail.com>
2026-09-07 09:22:24 -0400

    Add LIVV theme background artwork

---
+++ src/lib/brand-backgrounds.ts
@@ -0,0 +1,5 @@
+/** Founder-supplied LIVV background artwork. */
+
+export const LIVV_BACKGROUND_LIGHT = "data:image/jpeg;base64,/9j/4QfsR...";
+
+export const LIVV_BACKGROUND_DARK = "data:image/webp;base64,UklGR...";
