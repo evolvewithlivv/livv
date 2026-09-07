3c8fa848262392599575a6c1630a3926074367fb
evolvewithlivv <evolvewithlivv@gmail.com>
2026-09-07 17:45:32 -0400

    Clean duplicate global stylesheet import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Plus_Jakarta_Sans, Syne } from "next/font/google";
 import "./globals.css";
-import "./pack-rip.css";
 import "./livv-background.css";
 import { ThemeShell } from "@/components/layout/theme-shell";
 
