06d659e70293c4620ac1083b76f34161faa7e5e9
evolvewithlivv <evolvewithlivv@gmail.com>
2026-09-07 09:23:51 -0400

    Remove unused placeholder background asset

--- src/lib/brand-backgrounds.ts
+++
@@ -1,5 +0,0 @@
-/** Founder-supplied LIVV background artwork. */
-
-export const LIVV_BACKGROUND_LIGHT = "data:image/jpeg;base64,/9j/4QfsR...";
-
-export const LIVV_BACKGROUND_DARK = "data:image/webp;base64,UklGR...";
