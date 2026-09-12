e1727bbd4507e9feebc5b3d7e8b05d40b28219a1
evolvewithlivv <evolvewithlivv@gmail.com>
2026-09-11 21:04:19 -0400

    chore: keep api routes out of search crawlers

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/home/", "/auth", "/onboarding"],
+      disallow: ["/home/", "/auth", "/onboarding", "/api/"],
     },
   };
 }
