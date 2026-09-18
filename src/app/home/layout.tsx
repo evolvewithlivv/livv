f2e32527189d8fd7e951a5ed367a7c368d01c0ff
evolvewithlivv <evolvewithlivv@gmail.com>
2026-09-18 07:45:53 -0400

    refactor: remove global pillars footer

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -10,24 +10,6 @@ export default function HomeLayout({ children }: { children: React.ReactNode }) 
         <div className="min-h-[100dvh] overflow-y-auto overscroll-y-contain pb-28 [-webkit-overflow-scrolling:touch]">
           <AppHeader />
           {children}
-          <footer className="livv-brand-footer" aria-label="LIVV">
-            {/* Official LIVV Pillars logo. */}
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src="/share-backgrounds/LIVV%20Pillars%20Logo%20-%20BLACK.PNG"
-              alt="LIVV pillars"
-              className="livv-footer-logo livv-footer-logo-light"
-              draggable={false}
-            />
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src="/share-backgrounds/LIVV%20Pillars%20Logo%20-%20WHITE.PNG"
-              alt=""
-              aria-hidden="true"
-              className="livv-footer-logo livv-footer-logo-dark"
-              draggable={false}
-            />
-          </footer>
         </div>
         <BottomNav />
         <MilestoneCeremony />
